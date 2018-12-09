c1887543692e6f863252816436d97b7ab2acc00d
William Fish <william.michael.fish@gmail.com>
2018-12-09 19:26:39 +0000

    Working with multiple processes exercise 8

---
+++ working_with_multiple_processes_8.exs
@@ -0,0 +1,63 @@
+defmodule Scheduler do
+  def run(num_processes, module, func, to_calculate) do
+    1..num_processes
+    |> Enum.map(fn _ -> spawn(module, func, [self()]) end)
+    |> schedule_processes(to_calculate, [])
+  end
+
+  defp schedule_processes(processes, queue, results) do
+    receive do
+      {:ready, pid} ->
+        case queue do
+          [] ->
+            send(pid, {:shutdown})
+
+            if length(processes) > 1 do
+              schedule_processes(List.delete(processes, pid), queue, results)
+            else
+              Enum.sort(results, fn {n1, _}, {n2, _} -> n1 <= n2 end)
+            end
+
+          [head | tail] ->
+            send(pid, {:fib, head, self()})
+            schedule_processes(processes, tail, results)
+        end
+
+      {:answer, number, result, _pid} ->
+        schedule_processes(processes, queue, [{number, result} | results])
+    end
+  end
+end
+
+defmodule FibSolver do
+  def fib(scheduler) do
+    send(scheduler, {:ready, self()})
+
+    receive do
+      {:fib, n, client} ->
+        send(client, {:answer, n, fib_calc(n), self()})
+        fib(scheduler)
+
+      {:shutdown} ->
+        exit(:normal)
+    end
+  end
+
+  # very inefficient, deliberately
+  defp fib_calc(0), do: 0
+  defp fib_calc(1), do: 1
+  defp fib_calc(n), do: fib_calc(n - 1) + fib_calc(n - 2)
+end
+
+to_process = List.duplicate(37, 20)
+
+Enum.each(1..10, fn num_processes ->
+  {time, result} = :timer.tc(Scheduler, :run, [num_processes, FibSolver, :fib, to_process])
+
+  if num_processes == 1 do
+    IO.inspect(result)
+    IO.puts("\n # time (s)")
+  end
+
+  :io.format("~2B ~.2f~n", [num_processes, time / 1_000_000.0])
+end)
